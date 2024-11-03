f235387fe6e8a42559fd90090b07ea065b2a448a
Samuel Fanči <samofanci@gmail.com>
2024-11-03 17:04:47 +0100

    selective_classifier

--- model/ft_selective.py
+++ model/ft_selective.py
@@ -1,10 +1,12 @@
+import ast
 from esm import run_training, get_esm, parser, create_loss
-from classifiers import LinearClassifier, TokenClassifierConfig
+from classifiers import SelectiveFinetuningClassifier, SelectiveFinetuningClassifierConfig
 
 def create_model(args):
     esm, tokenizer = get_esm(args.type)
-    config = TokenClassifierConfig(n_labels=1, loss=create_loss(args))
-    model = LinearClassifier(base_model=esm, config=config)
+    indices = ast.literal_eval(args.indices)
+    config = SelectiveFinetuningClassifierConfig(n_labels=1, loss=create_loss(args), unfreeze_indices=indices)
+    model = SelectiveFinetuningClassifier(base_model=esm, config=config)
 
     return model, tokenizer
 
@@ -12,6 +14,6 @@ def main(args):
     run_training(args, create_model)
 
 if __name__ == '__main__':
-    # parser.add_argument('--focal_loss', action='store_true', default=False, help='Use focal loss')
+    parser.add_argument('--indices', default="[-1]", default=False, help='Indices of base model layers to be unfrozen')
     args = parser.parse_args()
     main(args)
